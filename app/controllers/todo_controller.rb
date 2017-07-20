0ba47e7342a5211802ae976e0d89f1df7a2194d1
Ryan Noble <nobleryan116@gmail.com>
2017-07-20 20:31:32 +0000

    Spiritually committed

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,13 +3,23 @@ class TodoController < ApplicationController
     end
     def show
         @todo = Todo.find_by_id(params[:id])
-        
-         
-         
-         
     end
-        
+    def edit
+        @todo = Todo.find_by_id(params[:id])
     end
+    def delete
+    end
+    def update
+        t = Todo.find_by_id(params[:id])
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro-estimate']
+        t.save
+        redirect_to "/todo/show/#{t.id}"
+        end
+    
+end
+        
+    
     
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
- 
-get'todo/index', to: 'todo#index'
+root to: 'todo#index'
 get'todo/show/:id', to: 'todo#show'
-
+get'todo/edit/:id', to: 'todo#edit'
+get'todo/delete/:id', to: 'todo#delete'
+get 'todo/update/:id', to: 'todo#update'
 
 
 
