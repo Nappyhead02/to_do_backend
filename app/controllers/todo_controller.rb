6a06e067ad0c724b878d5f782edb9f78ac25acc3
Ryan Noble <nobleryan116@gmail.com>
2017-07-20 18:04:48 +0000

    committment

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,8 +2,16 @@ class TodoController < ApplicationController
     def index
     end
     def show
-    @todo_description= "eat"
-    @todo_pomodoro_estimate= 8
+        todo_id = params[:id]
+        if todo_id == "1"
+            @todo_description = "sleep"
+            @todo_pomodoro_estimate = 8
+            elsif todo_id == "2"
+        @todo_description = "chill"
+         @todo_pomodoro_estimate = 6
+         
+    end
+        
     end
     
 end
